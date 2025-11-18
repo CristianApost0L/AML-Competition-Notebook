17fa7deac733978a3ff976758388668207816372
Soykat Amin <soykatamin@gmail.com>
2025-11-18 16:11:07 +0100

    checkpoints loading function sub 1

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import random
+from src.models.mlp_direct import ResidualMLP_BN, SwiGLUMLP
 
 def set_seed(seed):
     """
@@ -23,3 +24,26 @@ def set_seed(seed):
 
     print(f"Seed set to {seed} for random, numpy, and torch.")
     return seed_worker
+
+def load_direct_ensemble(device, model_paths):
+    models = []
+
+    # ------------------- Model 1 -------------------
+    m1 = ResidualMLP_BN(num_layers=3, dropout=0.3, hidden_dim=1536).to(device)
+    m1.load_state_dict(torch.load(model_paths[0], map_location=device))
+    m1.eval()
+    models.append(m1)
+
+    # ------------------- Model 2 -------------------
+    m2 = ResidualMLP_BN(num_layers=2, dropout=0.4, hidden_dim=2048).to(device)
+    m2.load_state_dict(torch.load(model_paths[1], map_location=device))
+    m2.eval()
+    models.append(m2)
+
+    # ------------------- Model 3 -------------------
+    m3 = SwiGLUMLP(num_layers=2, dropout=0.4, hidden_dim=1536).to(device)
+    m3.load_state_dict(torch.load(model_paths[2], map_location=device))
+    m3.eval()
+    models.append(m3)
+
+    return models
